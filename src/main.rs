1a9276743b8e698048f4d0320e62261d595ee6d6
L <>
2023-03-07 17:45:00 +0100

    unstable: wget ok, but not parssing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
-// use std::fs::File;
-// use std::io::prelude::*;
-
 use select::document::Document;
 use select::predicate::*;
-fn main(){
+
+extern crate reqwest;
+
+#[tokio::main]
+async fn main(){
+
+    let url = "https://leaderboards.companyofheroes.com/";
+
+    let resp = reqwest::get(url).await.unwrap();
+    assert!(resp.status().is_success());
+
+    let cat  = resp.text().await.unwrap();
+
+    let ccat = cat.as_str();
+
+    // println!("{}",ccat);
+
+    // #[tokio::main]
+    // async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //     let resp = reqwest::get("https://httpbin.org/ip")
+    //         .await?
+    //         .json::<HashMap<String, String>>()
+    //         .await?;
+    //     println!("{:#?}", resp);
+    //     Ok(())
+    // }
 
     // let mut file = File::open("Coh3exemple.html")?;
     // let mut html = String::new();
     // file.read_to_string(&mut html)?;
     
-    let htmlTXT = include_str!("Coh3exemple.html");
+    // let htmlTXT = resp.;//include_str!("Coh3exemple.html");
     
-    let doc = Document::from(htmlTXT);
+    let doc = Document::from(ccat);
 
-    for node in doc.find(Attr("class", "even:bg-[#131513]").descendant(Name("td"))) {
-        println!("{} ({:?})\n", node.text(), node.attr("class").unwrap());
+    for node in doc.find(Attr("class", "even:bg-[#131513]")) {
+        println!("{}",node.text());
+        
+        for nono in node.children(){
+            print!("{},", nono.text());
+        }
     }
-
+    
 }
 
 
