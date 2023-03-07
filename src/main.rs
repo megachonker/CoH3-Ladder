700323d08479ea6f5d2977202dc5a95ca575ce7a
megachonker <>
2023-03-07 18:22:31 +0100

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-// use std::fs::File;
-// use std::io::prelude::*;
-
 use select::document::Document;
 use select::predicate::*;
 fn main(){
 
-    // let mut file = File::open("Coh3exemple.html")?;
-    // let mut html = String::new();
-    // file.read_to_string(&mut html)?;
-    
     let htmlTXT = include_str!("Coh3exemple.html");
     
     let doc = Document::from(htmlTXT);
 
-    for node in doc.find(Attr("class", "even:bg-[#131513]").descendant(Name("td"))) {
-        println!("{} ({:?})\n", node.text(), node.attr("class").unwrap());
+    for node in doc.find(Class("even:bg-[#131513]").descendant(Name("td"))) {
+        for child in node.children() {
+            println!("{}",child.text());
+        }
     }
-
 }
 
 
