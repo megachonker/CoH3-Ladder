73214250bc709ca34818f35485f7c6fea8e328c4
megachonker <>
2023-03-09 23:57:20 +0100

    reading from input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use serde_json::Value;
 use std::error::Error;
-
+use std::env;
 async fn getpage(number : u64) -> Result<(), Box<dyn Error>>{
     // https://coh3-api.reliclink.com/community/leaderboard/getleaderboard2?count=50&leaderboard_id=2130306&start=2551&sortBy=1&title=coh3
-    let url = format!("https://coh3-api.reliclink.com/community/leaderboard/getleaderboard2?count=16&leaderboard_id=2130306&start={}&sortBy=1&title=coh3",number);
+    let url = format!("https://coh3-api.reliclink.com/community/leaderboard/getleaderboard2?count=200&leaderboard_id=2130306&start={}&sortBy=1&title=coh3",number);
     let json_str = reqwest::get(url)
     .await?
     .text()
@@ -15,20 +15,43 @@ async fn getpage(number : u64) -> Result<(), Box<dyn Error>>{
     //get number of player
     let size = v["statGroups"].as_array().unwrap().len();
 
-    for test  in 0..size  {
-        //bind variable
-        let player = &v["statGroups"][test]["members"][0];
-        let game = &v["leaderboardStats"][test];
 
-        //access var
-        print!("{} from {} ",player["alias"],player["country"]);
-        println!("Win/Loss {}/{}",game["wins"],game["losses"]);
+
+    for statgroups_indice  in 0..size  {
+        
+        //bind variable
+        let user_id = &v["statGroups"][statgroups_indice]["id"];
+
+        for laderboard_indice in 0..size  {
+            let statgroup_id = &v["leaderboardStats"][laderboard_indice]["statgroup_id"];
+
+            if statgroup_id == user_id{
+                // print!("{}vs{}",statgroup_id,user_id);
+                let player = &v["statGroups"][statgroups_indice]["members"][0];
+                let game = &v["leaderboardStats"][laderboard_indice];
+                print!("ELO {}: {} from {} ",game["rating"],player["alias"],player["country"]);
+                println!("Win/Loss {}/{}",game["wins"],game["losses"]);
+            }
+        }
+
+
+        // //access var
+        // print!("{} from {} ",player["alias"],player["country"]);
+        // println!("Win/Loss {}/{}",game["wins"],game["losses"]);
     }
     Ok(())
 }
 
 #[tokio::main]
 async fn main()  {
-    // getpage(1).await.unwrap();
-    getpage(2551).await.unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide at least one argument");
+        return;
+    }
+    let arg = &args[1];
+    let first_number:u64 = arg.chars().filter(|c| c.is_digit(10)).collect::<String>().parse::<u64>().unwrap_or(0);
+    println!("page number: {}", first_number);
+
+    getpage(first_number).await.unwrap();
 }
